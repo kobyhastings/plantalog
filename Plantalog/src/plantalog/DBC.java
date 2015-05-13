e401fecbccc29042bef8016ed1bc625421b2c83b
smikulcik <smikulcik@gmail.com>
2015-05-13 15:25:08 -0400

    forgot file

--- Plantalog/src/plantalog/DBC.java
+++ Plantalog/src/plantalog/DBC.java
@@ -24,9 +24,21 @@ public class DBC {
      * Sets up connection and statement
      */
     public static void connect(){
-        // URL for the database including the protocol (jdbc), the vendor 
-        //(oracle), the driver (thin), the server (csshrpt.eku.edu), and 
-        //the port number (1521)
+        String hostname, username, password, database, jdbcDriver, protocol, jdbcUrl;
+        if(true){//use mysql
+            hostname = "107.170.143.74";
+            username = "plantalog";
+            password = "plantalog123!";
+            database = "plantalog";
+            jdbcDriver = "com.mysql.jdbc.Driver";
+            protocol = "jdbc:mysql:";
+            jdbcUrl = protocol + "//"+hostname+"/"+database+"?username="+username+"&password="+password;  
+        }else{//use oracle
+            username = "mikulcik2015";
+            password = "8213";
+            jdbcDriver = "oracle.jdbc.driver.OracleDriver";
+            jdbcUrl = "jdbc:oracle:thin:@//csshrpt.eku.edu:1521/cscdb";
+        }
 
         try{
             Class.forName(jdbcDriver);
